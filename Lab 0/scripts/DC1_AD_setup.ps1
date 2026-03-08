ca1538ba3efcb450eb053287734f8c57ba2d4a57
Lasha Ghurtskaia <lashagurtskaia33@gmail.com>
2026-03-08 20:27:40 +0400

    Replace Ethernet configuration with AD DS setup

--- Lab 0/scripts/DC1_AD_setup.ps1
+++ Lab 0/scripts/DC1_AD_setup.ps1
@@ -1,18 +1,16 @@
-$interface_name = "Ethernet"
-$ethernet_index = (Get-NetAdapter -Name $interface_name).ifIndex;
-if ($ethernet_index -ge 1){
-    New-NetIPAddress -IPAddress 10.0.0.31 `
-				     -PrefixLength 24 `
-				     -InterfaceIndex $ethernet_index `
-				     | Out-Null
-    Set-DnsClientServerAddress -InterfaceIndex $ethernet_index `
-					 -ServerAddresses 10.0.0.31 `
-					 | Out-Null
-    Disable-NetAdapterBinding -Name $interface_name `
-				     -ComponentID ms_tcpip6 `
-				     | Out-Null
-    Write-Host "Ethernet configuration succeeded"
-    Get-NetAdapter
+try{
+    Install-WindowsFeature AD-Domain-Services -IncludeManagementTools 
+    Write-Host "AD DS installed successfully"
+} catch {
+    Write-Host "AD DS installation failed"
     exit 0
 }
-Write-Host "Ethernet configuration failed"
+$domainname = "corp.local"
+Import-Module ADDSDeployment
+Try{
+    Install-ADDSForest -DomainName $domainname -InstallDNS
+    Write-Host "AD DS Forest created successfully"
+} Catch {
+    Write-Host "AD DS Forest creation failed"
+    exit 0
+}
