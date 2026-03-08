0fc57cf9f28f4a7b69cee9b75d290ba5bbe7b424
Lasha Ghurtskaia <lashagurtskaia33@gmail.com>
2026-03-08 20:26:20 +0400

    Add DHCP installation and configuration to script

--- Lab 0/scripts/DC1_DHCP_setup.ps1
+++ Lab 0/scripts/DC1_DHCP_setup.ps1
@@ -1,16 +1,17 @@
 try{
-    Install-WindowsFeature AD-Domain-Services -IncludeManagementTools 
-    Write-Host "AD DS installed successfully"
+    Install-WindowsFeature DHCP -IncludeManagementTools -ErrorAction Stop
+    Write-Host "DHCP installation succeeded"
 } catch {
-    Write-Host "AD DS installation failed"
-    exit 0
+    Write-Host "DHCP installation failed $($_.Exception.Message)"
+    exit 1
 }
-$domainname = "corp.local"
-Import-Module ADDSDeployment
-Try{
-    Install-ADDSForest -DomainName $domainname -InstallDNS
-    Write-Host "AD DS Forest created successfully"
-} Catch {
-    Write-Host "AD DS Forest creation failed"
-    exit 0
+try{
+    Add-DhcpServerv4Scope -Name "Sales-Office01" -StartRange 10.00.00.100 -EndRange 10.00.00.200 -SubnetMask 255.255.255.0 -ErrorAction Stop
+    Set-DhcpServerv4OptionValue -ScopeId 10.0.0.0 -DnsServer 10.0.0.31 -ErrorAction Stop
+    Set-DhcpServerv4Scope -ScopeId 10.0.0.0 -State Active -ErrorAction Stop
+    Add-DhcpServerInDC -DnsName "DC1.corp.local" -IPAddress 10.0.0.31 -ErrorAction Stop
+    Write-Host "DHCP configuration succeeded"
+} catch {
+    Write-Host "DHCP configuration failed: $($_.Exception.Message)"
+    exit 1
 }
